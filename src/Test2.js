adccd0d3de83bde81f992f5f3f467748cfab13ca
Anton Serdyuchenko <anton415@gmail.com>
2019-04-28 11:26:35 +0300

    Callback Refs for task 2.

--- src/Test2.js
+++ src/Test2.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react'
 import Video from './video/video.mp4'
 
 class Test2 extends Component {
-  videoRef = React.createRef()
+  videoRef = null
+
+  setVideoRef = element => {
+    this.videoRef = element
+  }
 
   rewindForward = () => {
-    const videoElement = this.videoRef.current
-    if (!videoElement) {
-      return;
-    }
-    videoElement.currentTime += 5
+    if (this.videoRef) this.videoRef.currentTime += 5
   }
 
   render() {
     return (
       <div className="Test2">
-        <video ref={this.videoRef} controls >
+        <video ref={this.setVideoRef} controls >
           <source src={Video} type="video/mp4" />
         </video>
         <br />
